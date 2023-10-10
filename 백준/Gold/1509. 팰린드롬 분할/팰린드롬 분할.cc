e7d1ebc719d574cb60566d16e66a2ab64e1ca522
kindpark <99117035+kindpark@users.noreply.github.com>
2023-10-10 21:21:30 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 36 ms, Memory: 8152 KB -BaekjoonHub

---
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool palin[2503][2503];
+int dp[2503];
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    string s; cin >> s;
+    int n = s.size();
+    s = " " + s;
+    for (int i = 1; i <= n; i++) palin[i][i] = true;
+    for (int i = 1; i < n; i++) palin[i][i + 1] = (s[i] == s[i + 1]);
+    for (int j = 2; j < n; j++) {
+        for (int i = 1; i + j <= n; i++) {
+            if (s[i] == s[i + j] && palin[i + 1][i + j - 1]) palin[i][i + j] = 1;
+        }
+    }
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = 1e9;
+        for (int j = 1; j <= i; j++) {
+            if (palin[j][i]) dp[i] = min(dp[i], dp[j - 1] + 1);
+        }
+    }
+    cout << dp[n];
+}
