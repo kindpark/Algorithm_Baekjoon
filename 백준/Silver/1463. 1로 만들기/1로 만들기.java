a695f5e68663c6a3a194257efe6608400930903b
kindpark <99117035+kindpark@users.noreply.github.com>
2023-10-10 21:21:19 +0900

    [Silver III] Title: 1로 만들기, Time: 220 ms, Memory: 21656 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int arr[] = new int[N+1];
+		arr[0] = 0;
+		arr[1] = 0;
+		for(int i = 2; i <= N; i++){
+			arr[i] = arr[i-1] + 1;
+			if(i % 2 == 0) {
+				arr[i] = Math.min(arr[i], arr[i/2] + 1);
+			}
+			if(i % 3 == 0) {
+				arr[i] = Math.min(arr[i], arr[i/3] + 1);
+			}
+		}
+		System.out.println(arr[N]);
+	}
+}
