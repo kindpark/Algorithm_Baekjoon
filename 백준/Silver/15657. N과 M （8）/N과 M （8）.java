733eb992577797b78dd0052fb1bc6ac6b6b2087d
kindpark <99117035+kindpark@users.noreply.github.com>
2023-10-10 22:34:31 +0900

    [Silver III] Title: N과 M (8), Time: 676 ms, Memory: 21572 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int n, m;
+	static int[] sort;
+	static Vector<Integer> arr;
+	public static void main(String[] args) throws IOException{
+		StringTokenizer st;
+		String str = br.readLine();
+		st = new StringTokenizer(str);
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		str = br.readLine();
+		st = new StringTokenizer(str);
+		sort = new int[n];
+		arr = new Vector<Integer>(n);
+		//정렬에 사용될 숫자를 저장하는 배열
+		for(int i = 0; i < n; i++) {
+			int p = Integer.parseInt(st.nextToken());
+			sort[i] = p;
+			arr.add(p);
+		}
+		//
+		Collections.sort(arr);
+		Arrays.sort(sort);
+		dfs(0, 0);
+	}	
+	public static void dfs(int start, int depth) {
+		if(depth == m) {
+			int p = 0;
+			int i = 0;
+			while(p < m) {
+				System.out.print(sort[p] + " ");
+				p++;
+			}
+			System.out.println();
+			return;
+		}
+		for(int i = start; i <n; i++) {
+			sort[depth] = arr.get(i);
+			dfs(i, depth+1);
+		}
+	}
+}
