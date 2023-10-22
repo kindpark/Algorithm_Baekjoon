2255ac643d75854c2f2e7c3d7b36ed24c45af9fc
kindpark <99117035+kindpark@users.noreply.github.com>
2023-10-23 04:32:43 +0900

    [Gold V] Title: 괄호의 값, Time: 156 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,33 @@
+s = input()
+stack = []
+tmp = 1
+res = 0
+for i in range(len(s)):
+  if s[i] == '(':
+    tmp *= 2
+    stack.append(s[i])
+  elif s[i] == '[':
+    tmp *= 3
+    stack.append(s[i])
+
+  elif s[i] == ')':
+    if not stack or stack[-1] == '[':
+      res = 0
+      break
+    if s[i-1] == '(':
+      res += tmp
+    tmp //= 2
+    stack.pop() # pop
+  
+  else:
+    if not stack or stack[-1] == '(':
+      res = 0
+      break
+    if s[i-1] == '[':
+      res += tmp
+    tmp //= 3
+    stack.pop() # pop
+
+if stack:
+  res = 0
+print(res)
