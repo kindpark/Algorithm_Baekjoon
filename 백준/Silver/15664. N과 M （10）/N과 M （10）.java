f88f2f0203dc76e5de9a75d8255c57847a415ca2
kindpark <99117035+kindpark@users.noreply.github.com>
2023-11-09 09:15:42 +0900

    [Silver II] Title: N과 M (10), Time: 140 ms, Memory: 15820 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	static int[] arr;
+	static int[] copy;
+	static boolean[] visit;
+	static int n, m;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		arr = new int[n];
+		copy = new int[n];
+		visit = new boolean[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		sb = new StringBuilder();
+		promise(0, 0);
+		System.out.println(sb.toString());
+	}
+	public static void promise(int u, int c) {
+		if(m == u) {
+			for(int i = 0; i < m; i++) {
+				sb.append(copy[i] + " ");
+			}
+			sb.append("\n");
+		}
+		int p = 0;
+		for(int i = c; i < n; i++) {
+			if(!visit[i] && arr[i] != p) {
+				copy[u] = arr[i];
+				p = copy[u];
+				visit[i] = true;
+				promise(u+1, i);
+				visit[i] = false;
+			}
+		}
+	}
+
+}
