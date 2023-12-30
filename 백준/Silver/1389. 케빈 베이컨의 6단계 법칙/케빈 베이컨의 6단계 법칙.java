c461274334189b6f9149ca52ede874bf7fcec97d
kindpark <99117035+kindpark@users.noreply.github.com>
2023-12-31 04:25:19 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 152 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,73 +1,64 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
-
-	static List<Integer>[] list;
-	static int[] memo;
+	static int[][] connect;
+	static int[] path;
+	static int[] bacon;
 	static boolean[] check;
-	static int answer;
-    
-	public static void main(String[] args) throws IOException{
+	static int n, m;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		memo = new int[n+1];
-		list = new ArrayList[n+1];
-		for(int i=0; i<n+1; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		for(int i=0; i<m; i++) {
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		connect = new int[n+1][n+1];
+		path = new int[n+1];
+		bacon = new int[n+1];
+		check = new boolean[n+1];
+		for(int i = 1; i <= m; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			list[a].add(b);
-			list[b].add(a);
+			int b= Integer.parseInt(st.nextToken());
+			connect[a][b] = connect[b][a] = 1;
 		}
+		for(int i = 1; i <= n; i++) {
+			bfs(i);
+			for(int j = 1; j <= n; j++) {
+				if(i!=j)
+					bacon[i] += path[j];
+				
+			}
+			for(int j = 1; j <= n; j++) {
+				path[j] = 0;
+				check[j] = false;
+			}
+		}
+		int minbc = bacon[1];
+		int ans = 1;
+		for(int i = 2; i <= n; i++) {
+			
+			if(minbc > bacon[i]) {
+				minbc = bacon[i];
+				ans = i;
+			}
+		}
+		System.out.println(ans);
+	}
+	public static void bfs(int start) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		check[start] = true;
 		
-		for(int i=1; i<n+1; i++) {
-			for(int j=1; j<n+1; j++) {
-				answer= Integer.MAX_VALUE; 
-				check = new boolean[n+1];
-				if(i!=j) {
-					dfs(i,j,0);
-					memo[i] += answer;
+		while(!q.isEmpty()) {
+			int a = q.poll();
+			for(int i = 1; i<=n; i++) {
+				if(connect[a][i] == 1 && !check[i]) {
+					check[i] = true;
+					q.add(i);
+					
+					path[i] = path[a]+1;
 				}
 			}
 		}
-		int min = Integer.MAX_VALUE;
-		int idx =0;
-		for(int i=1; i<n+1; i++) {
-			if(min > memo[i]) {
-				min = memo[i];
-				idx =i;
-			}
-		}
-		System.out.println(idx);
-		
-		
-	}
-	
-	
-	static void dfs(int start, int end, int cnt) {
-		if(start == end) {
-			answer = Math.min(answer, cnt);
-			return;
-		}
-		
-		check[start] = true;
-		for(int i=0; i<list[start].size(); i++) {
-			int next = list[start].get(i);
-			if(!check[next]) {
-				dfs(next,end, cnt+1);
-			}
-		}
-		check[start]=false;
 	}
 }
