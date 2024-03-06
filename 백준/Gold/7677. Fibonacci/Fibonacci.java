7e3ff6589b5fc25ec19e890da7fff46eb8a222ba
kindpark <99117035+kindpark@users.noreply.github.com>
2024-03-06 10:23:56 +0900

    [Gold II] Title: Fibonacci, Time: 132 ms, Memory: 14740 KB -BaekjoonHub

---
+++ 백준/Gold/7677. Fibonacci/Fibonacci.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static long[][] fibo = {{1, 1},{1, 0}};
+    public static final int MOD = 1000000007;
+
+    public static void main(String[] args) throws Exception {
+        long[][] res;
+        while(true) {
+        	long n = Long.parseLong(br.readLine());
+        	if(n == -1) {
+        		break;
+        	}
+        	else {
+        		res = matPow(fibo, n);
+        		System.out.println(res[1][0]);
+        	}
+        }
+        br.close();
+    }
+
+    public static long[][] matPow(long[][] tmp, long exp) {
+        if(exp == 1) {
+            return tmp;
+        } else if(exp == 0) {
+            long[][] zero = {{0, 0},{0, 0}};
+            return zero;
+        }
+
+        long[][] res = matPow(tmp, exp / 2);
+
+        if(exp % 2 == 1) {
+            return matMul(matMul(res, res), tmp);
+        } else {
+            return matMul(res, res);
+        }
+    }
+
+    public static long[][] matMul(long[][] tmp1, long[][] tmp2) {
+        long[][] res = new long[2][2];
+
+        res[0][0] = ( (tmp1[0][0] * tmp2[0][0]) + (tmp1[0][1] * tmp2[1][0]) ) % 10000;
+        res[0][1] = ( (tmp1[0][0] * tmp2[0][1]) + (tmp1[0][1] * tmp2[1][1]) ) % 10000;
+        res[1][0] = ( (tmp1[1][0] * tmp2[0][0]) + (tmp1[1][1] * tmp2[1][0]) ) % 10000;
+        res[1][1] = ( (tmp1[1][0] * tmp2[0][1]) + (tmp1[1][1] * tmp2[1][1]) ) % 10000;
+
+        return res;
+    }
+}
