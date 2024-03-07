dc3cd97cf732a5b77f7c6b35d6ce6771fb38a734
kindpark <99117035+kindpark@users.noreply.github.com>
2024-03-07 16:21:57 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 236 ms, Memory: 19532 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -1,98 +1,31 @@
 import java.io.*;
-
-import java.util.StringTokenizer;
-//[참고] 자바 문제 6) 제곱 ㄴㄴ 수 - BAEKJOON 1016|작성자 repid2
- 
-
+import java.util.*;
 public class Main {
-
- 
-
     public static void main(String[] args) throws Exception {
-
-        BufferedReader br = 
-
-                new BufferedReader(new InputStreamReader(System.in));
-
-        BufferedWriter bw = 
-
-                new BufferedWriter(new OutputStreamWriter(System.out));
-
-
-
- 
-
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
- 
-
         long min = Long.parseLong(st.nextToken());
-
         long max = Long.parseLong(st.nextToken());
-
- 
-
- 
-
         int result = (int) (max - min) + 1;
-
- 
-
         boolean[] check = new boolean[result];
 
- 
-
         for (long i = 2; i * i <= max; i++) {
-
             long squared = i * i; 
-
-            long start = min % squared == 0 ? min / 
-
-                        squared : (min / squared) + 1;    
-
-
-        
-
- 
+            long start = min % squared == 0 ? min / squared : (min / squared) + 1;    
 
             for (long j = start; squared * j <= max; j++) {
-
                 check[(int) ((j * squared) - min)] = true;
-
-
-                
-
-       
-
             } 
-
- 
-
         }
         int count = 0;
-
-        
-
         for (int i = 0; i <= max - min; i++) {
-
             if (!check[i])
-
                 count++;
-
         }
-
         bw.write(count + "\n");
-
- 
-
         bw.flush();
-
         br.close();
-
         bw.close();
-
- 
-
     }
-
 }
