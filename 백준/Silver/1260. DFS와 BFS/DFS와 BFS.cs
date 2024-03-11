07651517cfc4a9e2638582043d194127771ada6c
kindpark <99117035+kindpark@users.noreply.github.com>
2024-03-11 17:27:27 +0900

    [Silver II] Title: DFS와 BFS, Time: 84 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cs
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cs
@@ -1,68 +1,62 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace ConsoleApp1 {
-    class Program
-    {
-        static int[,] array;
-        static int n, m, v;
-        static Queue<int> q = new Queue<int>();
-        static bool[] check;
-        static void Main(String[] args)
-        {
-            string q = Console.ReadLine();
-            string[] split = q.Split(' ');
-            n = Convert.ToInt32(split[0]);
-            m = Convert.ToInt32(split[1]);
-            v = Convert.ToInt32(split[2]);
+class MainClass {
+    static int[,] li;
+    static bool[] check;
+    static System.Text.StringBuilder sb;
+    static Queue<int> q;
+    static int n, m, v;
 
-            array = new int[n + 1, n + 1];
-            check = new bool[n + 1];
-            for (int i = 0; i < m; i++)
-            {
-                int[] mArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-                int a = Convert.ToInt32(mArray[0]);
-                int b = Convert.ToInt32(mArray[1]);
-                array[a, b] = 1;
-                array[b, a] = 1;
-            }
-            dfs(v);
-            Console.WriteLine();
-            check = new bool[n + 1];
-            bfs(v);
-        }
-        static void dfs(int start)
-        {
-            check[start] = true;
-            Console.Write(start + " ");
-            for(int i = 1; i <= n; i++)
-            {
-                if (!check[i] && array[start,i] == 1)
-                {
-                    dfs(i);
+    public static void bfs(int v) {
+        q.Enqueue(v);
+        check[v] = true;
+        while (q.Count > 0) {
+            v = q.Dequeue();
+            sb.Append(v + " ");
+            for (int i = 1; i <= n; i++) {
+                if (!check[i] && li[v, i] == 1) {
+                    q.Enqueue(i);
+                    check[i] = true;
                 }
             }
         }
-        static void bfs(int start)
-        {
-            q.Enqueue(start);
-            check[start] = true;
-            while (q.TryPeek(out int result))
-            {
-                start = q.Dequeue();
-                Console.Write(start + " ");
-                for (int i = 1; i <=n; i++)
-                {
-                    if(!check[i] && array[start, i] == 1)
-                    {
-                        q.Enqueue(i);
-                        check[i] = true;
-                    }
-                }
+    }
+
+    public static void dfs(int v) {
+        check[v] = true;
+        sb.Append(v + " ");
+        for (int i = 1; i <= n; i++) {
+            if (!check[i] && li[v, i] == 1) {
+                dfs(i);
             }
         }
+    }
+
+    public static void Main(string[] args) {
+        StreamReader sr = new StreamReader(Console.OpenStandardInput());
+        sb = new System.Text.StringBuilder();
+        string[] input = sr.ReadLine().Split(' ');
+        n = int.Parse(input[0]);
+        m = int.Parse(input[1]);
+        v = int.Parse(input[2]);
+        li = new int[n + 1, n + 1];
+        check = new bool[n + 1];
+        q = new Queue<int>();
+        for (int i = 0; i < m; i++) {
+            string[] s = sr.ReadLine().Split(' ');
+            int x = int.Parse(s[0]);
+            int y = int.Parse(s[1]);
+
+            li[x, y] = 1;
+            li[y, x] = 1;
+        }
+        dfs(v);
+        Console.WriteLine(sb);
+        sb = new System.Text.StringBuilder();
+        check = new bool[n + 1];
+        bfs(v);
+        Console.WriteLine(sb);
     }
 }
