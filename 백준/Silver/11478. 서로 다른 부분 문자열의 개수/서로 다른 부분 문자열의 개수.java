9c02a7b61cdd870bd48938d42c1c36cc078517e6
kindpark <99117035+kindpark@users.noreply.github.com>
2024-03-18 13:44:49 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 684 ms, Memory: 222204 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -1,10 +1,10 @@
 import java.util.*;
 import java.io.*;
-public class Main {
+public class Main{
 	public static void main(String[] args) throws IOException{
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		TreeSet<String> ts = new TreeSet<>();
-		String s = sc.nextLine();
+		String s = br.readLine();
 		for(int i = 0; i < s.length(); i++) {
 			for(int j = i; j <= s.length(); j++) {
 				ts.add(s.substring(i, j));
