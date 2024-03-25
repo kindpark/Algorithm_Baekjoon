129ca0beaeae87b30b1f2de1d8f75744f1d324f2
kindpark <99117035+kindpark@users.noreply.github.com>
2024-03-25 12:56:04 +0900

    [Gold V] Title: 애너그램, Time: 1612 ms, Memory: 29884 KB -BaekjoonHub

---
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	static int n;
+	static char[] grammer;
+	static int[] check;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		check = new int[26];
+		for(int i = 0; i < n; i++) {
+			String g = br.readLine();
+			grammer = new char[g.length()];
+			for(int j = 0; j < g.length(); j++) {
+				check[g.charAt(j) - 'a']++;
+			}
+			dfs(g.length(), 0);
+			Arrays.fill(check, 0);
+		}
+	}
+	public static void dfs(int length, int x) {
+		if(x == length) {
+			System.out.println(grammer);
+			return;
+		}
+		else {
+			for(int i = 0; i < 26; i++) {
+				if(check[i] > 0) {
+					check[i]--;
+					grammer[x] = (char)(i+'a');
+					dfs(length, x+1);
+					//중복된 단어 체킹
+					check[i]++;
+				}
+			}
+		}
+	}
+}
