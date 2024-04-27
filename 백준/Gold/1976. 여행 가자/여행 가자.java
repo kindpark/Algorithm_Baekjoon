bd0cc7aa4d86a3ba7aa221ca32c93668d1369b7d
kindpark <99117035+kindpark@users.noreply.github.com>
2024-04-28 06:09:23 +0900

    [Gold IV] Title: 여행 가자, Time: 180 ms, Memory: 16836 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,66 +1,50 @@
-import java.io.*;
 import java.util.*;
- 
+import java.io.*;
 public class Main {
-    static int[] parent;
- 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
-        }
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                int temp = Integer.parseInt(st.nextToken());
-                if (temp == 1) {
-                    union(i, j);
-                }
-            }
-        }
- 
-        st = new StringTokenizer(br.readLine());
-        int start = find(Integer.parseInt(st.nextToken()));
-        for (int i = 1; i < M; i++) {
-            int now = Integer.parseInt(st.nextToken());
-            if (start != find(now)) {
-                bw.write("NO\n");
-                bw.flush();
-                bw.close();
-                br.close();
-                return;
-            }
-        }
- 
-        bw.write("YES\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    public static int find(int x) {
-        if (x == parent[x]) {
-            return x;
-        }
- 
-        return parent[x] = find(parent[x]);
-    }
-
-    public static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
- 
-        if (x != y) {
-            if (x < y) {
-                parent[y] = x;
-            } else {
-                parent[x] = y;
-            }
-        }
-    }
- 
+	static int[] verse;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		verse = new int[n+1];
+        for(int i = 1; i <= n; i++) {
+			verse[i] = i;
+		}
+		StringTokenizer st;
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= n; j++) {
+				int c = Integer.parseInt(st.nextToken());
+				if(c == 1) {
+					union(i, j);
+				}
+			}
+		}
+		st = new StringTokenizer(br.readLine());
+		int start = find(Integer.parseInt(st.nextToken()));
+		
+		for(int i = 1; i < m; i++) {
+			int now = Integer.parseInt(st.nextToken());
+			if(start != find(now)){
+				System.out.println("NO");
+				br.close();
+				return;
+			}
+		}
+		System.out.println("YES");
+		br.close();
+	}
+	public static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if(x<y) verse[y] = x;
+		else verse[x] = y;
+	}
+	public static int find(int x) {
+		if(verse[x] == x) return x;
+		else return verse[x] = find(verse[x]);
+	}
+	public static boolean isUnion(int x, int y) {
+		return find(x)== find(y);
+	}
 }
