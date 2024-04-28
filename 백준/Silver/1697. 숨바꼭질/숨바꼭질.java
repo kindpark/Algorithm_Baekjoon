f0e160197ce283486ea118b7a7c6be489d464878
kindpark <99117035+kindpark@users.noreply.github.com>
2024-04-29 02:27:45 +0900

    [Silver I] Title: 숨바꼭질, Time: 332 ms, Memory: 88584 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	static boolean[] visit;
+	static int n, k; 
+	static long sec;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		k = sc.nextInt();
+		visit = new boolean[10000001];
+		bfs(n);
+		System.out.println(sec);
+		
+	}
+	public static void bfs(int n) {
+		Queue<Node> q = new LinkedList<>();
+		q.add(new Node(n, 0));
+		while(!q.isEmpty()) {
+			Node nd = q.poll();
+			visit[nd.n] = true;
+			if(nd.n == k) {
+				sec = nd.sec;
+				break;
+			}
+			if(nd.n*2 >= 0)
+			if(nd.n * 2 <= 100000 && !visit[nd.n*2]) {
+				q.add(new Node(nd.n * 2, nd.sec+1));
+			}
+			if(nd.n+1 >= 0)
+			if(nd.n + 1 <= 100000 && !visit[nd.n+1]) {
+				q.add(new Node(nd.n + 1, nd.sec+1));
+			}
+			if(nd.n-1 >= 0)
+			if(nd.n - 1 <= 100000 && !visit[nd.n-1]) {
+				q.add(new Node(nd.n-1, nd.sec+1));
+			}
+		}
+	}
+	static class Node {
+		int n;
+		int sec;
+		Node(int n, int sec){
+			this.n = n;
+			this.sec = sec;
+		}
+	}
+}
