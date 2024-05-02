fa2f1d6fa5c1769e92fcc29b7f38f2d73d0bae29
kindpark <99117035+kindpark@users.noreply.github.com>
2024-05-02 16:48:13 +0900

    [Gold IV] Title: 지뢰찾기, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Gold/2140. 지뢰찾기/지뢰찾기.java
+++ 백준/Gold/2140. 지뢰찾기/지뢰찾기.java
@@ -1,61 +1,56 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    static int N;
-    static char map[][];
+	static int n, res=0;
+	static char[][] board;
     static int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
     static int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new char[N][N];
-
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                map[i][j] = str.charAt(j);
-            }
-        }
-
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (Character.isDigit(map[i][j])) {
-                    int cur = map[i][j] - '0';
-                    check(i, j, cur);
-                }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == '*' || map[i][j] == '#') {
-                    cnt++;
-                }
-            }
-        }
-
-        System.out.println(cnt);
-    }
-
-    /* 8방향 체크 */
-    public static void check(int x, int y, int cur) {
-        for (int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-
-            if (map[nx][ny] == '#' && cur != 0) {
-                cur--;
-                map[nx][ny] = '*';
-            } else if (map[nx][ny] == '*' && cur != 0) {
-                cur--;
-            } else if (map[nx][ny] == '#' && cur == 0) {
-                map[nx][ny] = '-';
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		n = Integer.parseInt(br.readLine());
+		board = new char[n][n];
+		for(int i = 0; i < n; i++) {
+			String s = br.readLine();
+			for(int j = 0; j < n; j++) {
+				board[i][j] = s.charAt(j);
+			}
+		}
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(Character.isDigit(board[i][j])) {
+					int cur = board[i][j] - '0';
+					search(i, j, cur);
+				}
+			}
+		}
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(board[i][j] == '*' || board[i][j] == '#') {
+					res++;
+				}
+			}
+		}
+		bw.write(String.valueOf(res));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	public static void search(int x, int y, int bomb) {
+		for(int i = 0; i < 8; i++) {
+			int px = dx[i] + x;
+			int py = dy[i] + y;
+			if(px < 0 || py < 0 || px >= n || py >= n) continue;
+			if(board[px][py] == '#' && bomb != 0) {
+				bomb--;
+				board[px][py] = '*';
+			}
+			else if(board[px][py] == '*' && bomb != 0) {
+				bomb--;
+			}
+			else if(board[px][py] == '#' && bomb == 0) {
+				board[px][py] = 'X';
+			}	
+		}
+	}
 }
