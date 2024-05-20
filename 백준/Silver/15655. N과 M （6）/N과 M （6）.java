241e7eb53543f1c1c15e41e6b5bd83b18f629f5f
kindpark <99117035+kindpark@users.noreply.github.com>
2024-05-20 16:58:34 +0900

    [Silver III] Title: N과 M (6), Time: 152 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -5,6 +5,7 @@ public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static int n, m;
 	static int[] sort;
+	static boolean[] visited;
 	static Vector<Integer> arr;
 	public static void main(String[] args) throws IOException{
 		StringTokenizer st;
@@ -15,6 +16,7 @@ public class Main {
 		str = br.readLine();
 		st = new StringTokenizer(str);
 		sort = new int[n];
+		visited = new boolean[n];
 		arr = new Vector<Integer>(n);
 		//정렬에 사용될 숫자를 저장하는 배열
 		for(int i = 0; i < n; i++) {
@@ -39,8 +41,13 @@ public class Main {
 			return;
 		}
 		for(int i = start; i <n; i++) {
+			if(visited[i]) {
+				continue;
+			}
+			visited[i] = true;
 			sort[depth] = arr.get(i);
 			dfs(i+1, depth+1);
+			visited[i] = false;
 		}
 	}
 }
