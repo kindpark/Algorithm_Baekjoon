0e7c73f2933b7907ace27a3c292f961e338d5f88
kindpark <99117035+kindpark@users.noreply.github.com>
2024-06-17 19:40:38 +0900

    [Gold V] Title: Coins, Time: 120 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Gold/3067. Coins/Coins.java
+++ 백준/Gold/3067. Coins/Coins.java
@@ -2,30 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static int[] a, b;
-    static int[] dp;
-    public static void main(String[] args) throws IOException{
+    static int t, n, m;
+    static int[] arr, money;
+
+    static ArrayList<Integer> list;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int t = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        while(t-- >0) {
+        t = Integer.parseInt(br.readLine());
+        while(t-->0){
             n = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-            a = new int[n+1];
-            for(int i = 1; i <= n; i++) {
-                a[i] = Integer.parseInt(st.nextToken());
+            money = new int[n+1];
+            for(int i = 1; i <= n; i++){
+                money[i] = Integer.parseInt(st.nextToken());
             }
             m = Integer.parseInt(br.readLine());
-            dp = new int[m+1];
-            dp[0] = 1;
+            arr = new int[m+1];
+            arr[0] = 1;
             for(int i = 1; i <= n; i++){
-                for(int j = a[i]; j <= m; j++){
-                    dp[j] = dp[j] + dp[j-a[i]];
+                for(int j = money[i]; j <= m; j++){
+                    arr[j] = arr[j] + arr[j-money[i]];
                 }
             }
-            bw.write(String.valueOf(dp[m])+'\n');
+            bw.write(String.valueOf(arr[m])+"\n");
         }
         bw.flush();
         bw.close();
