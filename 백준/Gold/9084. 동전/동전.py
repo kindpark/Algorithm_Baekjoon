63e0ed89c1f90451704c6d7802912868cc95a527
kindpark <99117035+kindpark@users.noreply.github.com>
2024-06-17 19:49:18 +0900

    [Gold V] Title: 동전, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    m = int(input())
+    dp = [0 for _ in range(m+1)]
+    dp[0] = 1
+    for i in range(1, n+1):
+        for j in range(arr[i-1], m+1):
+            dp[j] = dp[j] + dp[j-arr[i-1]]
+    print(dp[m])
